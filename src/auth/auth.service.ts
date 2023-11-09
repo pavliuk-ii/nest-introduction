84fe56ed4560413d848d3ca38d7158d3bf7b1ecc
Ihor Pavliuk <nemuzora.nozomu@gmail.com>
2023-11-09 15:12:11 +0200

    added basic logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  login() {}
+  signup() {
+    return { msg: "hello, I'm signed up" };
+  }
 
-  signup() {}
+  signin() {
+    return { msg: "hello, I'm signed in" };
+  }
 }
